38af7eff32619478afce85e79b29ba7253587734
kandelarjun67 <kandelarjun67@gmail.com>
2023-04-24 17:57:18 +0545

    updating the index.js file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("test")
+const app = require('express')()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Gantabya!')
+})
+
+app.listen(port, () => {
+    console.log(`Welcome to Gantabya ${port}`)
+})
