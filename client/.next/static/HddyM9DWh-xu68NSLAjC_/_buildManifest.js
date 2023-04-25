4581b942e74f85dd1968823b5fddaef288660525
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-04-25 09:10:10 +0545

    nextjs client setup

---
+++ client/.next/static/HddyM9DWh-xu68NSLAjC_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3f909ba5d96efd38.css","static/chunks/pages/index-6ce781b379a97850.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
