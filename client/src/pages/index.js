88056cc205a932637d13ed154acafc9413bc9f11
Rajat Pokhrel <pokhrelrajat@gmail.com>
2023-04-30 08:30:32 +0545

    updated registration and login pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,30 +1,92 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import {useState } from 'react';
-import styles from '@/styles/Home.module.css'
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-const inter = Inter({ subsets: ['latin'] })
+const LoginSchema = Yup.object().shape({
+  phoneNumber: Yup.string()
+    .min(10, "Too Short")
+    .max(15, "Too Long")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "too short")
+    .max(20, "too Long")
+    .required("Required"),
+  role: Yup.string().required("Required"),
+});
 
-const Home = ()=> {
-  const [ phoneNumber, setPhoneNumber] = useState('')
-  const [ password, setPassword] = useState('')
-
-  const handleLogin = async()=>{
+const Home = () => {
+  const handleLogin = async () => {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ phoneNumber: phoneNumber, password:password })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: "username and password received" }),
+    };
+    try {
+      const data = await fetch("http://localhost:4000/login", requestOptions);
+      if (data) {
+        res.JSON(data);
+      } else {
+        res.JSON(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
-    const data = await fetch('http://localhost:4000/login',requestOptions)
-  }
+
   return (
     <>
-            <input placeholder="Mobile Number" onChange={(e)=>setPhoneNumber(e.target.value)}/>
-            <input placeholder="password" onChange={(e)=>setPassword(e.target.value)}/>
-            <button onClick={handleLogin}>Login</button>
-    </>
-  )
-}
+      <div className="container">
+        <div>
+          <h1>Login</h1>
+          <Formik
+            initialValues={{
+              phoneNumber: "",
+              password: "",
+              role: "",
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => {
+              handleLogin();
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form className="formContainer">
+                {errors.phoneNumber && touched.phoneNumber ? (
+                  <div className="formError">{errors.phoneNumber} !</div>
+                ) : null}
+                <Field
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  className="formItems"
+                />
 
-export default Home
+                {errors.password && touched.password ? (
+                  <div className="formError">{errors.password} !</div>
+                ) : null}
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className="formItems"
+                />
+
+                {errors.role && touched.role ? (
+                  <div className="formError">
+                    http://localhost:3000/{errors.role} !
+                  </div>
+                ) : null}
+                <Field name="role" placeholder="Role" className="formItems" />
+
+                <button type="submit" className="myButton">
+                  Submit
+                </button>
+              </Form>
+            )}
+            <p>Dont have an account?</p>
+          </Formik>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,15 +1,103 @@
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const RegisterSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .min(5, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  phoneNumber: Yup.string()
+    .min(10, "Too Short")
+    .max(15, "Too Long")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "too short")
+    .max(20, "too Long")
+    .required("Required"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+  role: Yup.string().required("Required"),
+});
+
 const Register = () => {
-    const fetchUserDetails = ()=> {
-        
-    }
-    return (
+  return (
+    <>
+      <div className="container">
         <div>
-            <input placeholder="Full Name" />
-            <input placeholder="Mobile Number" />
-            <input placeholder="password"/>
-            <input placeholder="role"/>
-            <button>Register</button>
+          <h1>Register</h1>
+          <Formik
+            initialValues={{
+              fullName: "",
+              phoneNumber: "",
+              password: "",
+              confirmPassword: "",
+              role: "",
+            }}
+            validationSchema={RegisterSchema}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form className="formContainer">
+                {errors.fullName && touched.fullName ? (
+                  <div className="formError">
+                    Full Name is {errors.fullName} !
+                  </div>
+                ) : null}
+                <Field
+                  name="fullName"
+                  placeholder="Full Name"
+                  className="formItems"
+                />
+
+                {errors.phoneNumber && touched.phoneNumber ? (
+                  <div className="formError">{errors.phoneNumber} !</div>
+                ) : null}
+                <Field
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  className="formItems"
+                />
+
+                {errors.password && touched.password ? (
+                  <div className="formError">
+                    Password is {errors.password} !
+                  </div>
+                ) : null}
+                <Field
+                  name="password"
+                  type="text"
+                  placeholder="Password"
+                  className="formItems"
+                />
+
+                {errors.confirmPassword && touched.confirmPassword ? (
+                  <div className="formError">{errors.confirmPassword} !</div>
+                ) : null}
+                <Field
+                  name="confirmPassword"
+                  type="text"
+                  placeholder="Confirm Password"
+                  className="formItems"
+                />
+
+                {errors.role && touched.role ? (
+                  <div className="formError">Role is {errors.role} !</div>
+                ) : null}
+                <Field name="role" placeholder="Role" className="formItems" />
+
+                <button type="submit" className="myButton">
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
-    )
-}
-export default Register
+      </div>
+    </>
+  );
+};
+export default Register;
