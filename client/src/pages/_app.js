e2a8593923a0a03bce06de6721d0ec3bb851fb24
sameer <febsec14@gmail.com>
2023-05-08 23:20:03 +0545

    login ui created

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from 'react-redux';
+import { wrapper } from '../redux/store/index';
+function App({ Component, ...rest }) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default App;
