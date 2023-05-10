ce92f8c49f3365f91ec566cb864279c1239400b8
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2023-05-10 19:12:37 +0545

    added some error handling cases

--- server/src/index.js
+++ server/src/index.js
@@ -39,32 +39,35 @@ app.use(express.json())
 
 
 app.post('/register', async(req, res) => {
-  try{
-const data = await Users.findOne({phoneNumber:req.body.phoneNumber })
-console.log(data)
-if(data){
-  res.json({
-    msg: "Already exist",
-    success:false
-  })
-}else{
-  const hash = await bcrypt.hash(req.body.password, 0)
-  console.log(hash)
-  if(hash){
-    req.body.password = hash
-    const data = await Users.create(req.body)
-    if(data) {
-      res.json({
-        msg: "Register success",
-        success:true
-
-      })
+  try {
+    const data = await Users.findOne({phoneNumber:req.body.phoneNumber })
+  console.log(data)
+  if(data){
+    res.json({
+      msg: "Already exist",
+      success:false
+    })
+  }else{
+    const hash = await bcrypt.hash(req.body.password, 0)
+    console.log(hash)
+    if(hash){
+      req.body.password = hash
+      const data = await Users.create(req.body)
+      if(data) {
+        res.json({
+          msg: "Register success",
+          success:true
+  
+        })
+      }
     }
   }
-} } catch(err){
-  message.error("Unable to find data in the database")
+    
+  } catch (error) {
+    message.error("Unable to get response from register")
+    
+  }
 
-}
 
 })
    
@@ -73,20 +76,24 @@ if(data){
 
 app.post('/login', async (req, res) => {
   //user found in db?
-  const data = await Users.findOne({phoneNumber: req.body.phoneNumber})
+  try{
+    const data = await Users.findOne({phoneNumber: req.body.phoneNumber})
   if(data){
           //user cred match
           const isMatched = await bcrypt.compare(req.body.password, data.password)
         if(isMatched) {
           //generete the token for this matched user and send the token as reponse
-          const token = jwt.sign({ phoneNumber: req.body.phoneNumber }, process.env.Secretkey);
+          const token = jwt.sign({ phoneNumber: req.body.phoneNumber }, process.env.SECRET_KEY);
           res.json({message: "login succcess", success:true, token: token})
         }else{
           res.json({message: "login failed",success:false})
         }
   }else{
     res.json({message: "user not exist",success:false})
-  }
+  }} catch(err){
+    message.error(res.json)
+  } 
+
  
 })
 
