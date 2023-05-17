16c6c95eb5d6d0823bd085505eeb7c0c83ba72ff
unknown <aeronmaharjan200@gmail.com>
2023-05-17 21:03:59 +0545

    added logo and background image in login page

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aedf922a3a2f8c1c"; }
+/******/ 		__webpack_require__.h = function() { return "40116781d6b80eb7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
