aba1f7adb44964498db12aafdad3d12041fc1afc
unknown <aeronmaharjan200@gmail.com>
2023-05-22 20:56:13 +0545

    Password hide/unhide in register page

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -6,58 +6,81 @@ import { Button, message, Switch } from 'antd';
 
 import styles from './Register.module.css';
 import { useDispatch, useSelector } from 'react-redux'
+import {RxEyeClosed,RxEyeOpen} from 'react-icons/rx'
 
-const SignupSchema = Yup.object().shape({
-  fullName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  password: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  phoneNumber: Yup.string()
-    .matches(/^\d{10}$/, 'Phone number must be 10 digits')
-    .required('Required'),
-  role: Yup.string(),
-
-});
 
 const Register = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const [isRider, setisRider] = useState(false);
-  const [file, setFile] = useState(null)
+  const [open, setOpen] = useState(false);
+let SignupSchema
+   
+    SignupSchema = isRider?(Yup.object().shape({
+   
+    role: Yup.string(),
+    
+  licenseNumber: Yup.string()
+  .min(2, "Too Short!")
+  .max(50, "Too Long!")
+  .required("Required"),
+vehicleNumber: Yup.string()
+  .min(2, "Too Short!")
+  .max(50, "Too Long!")
+  .required("Required"),
+vehicleType: Yup.string().required("Required"),
+  
+  })):(Yup.object().shape({
+    fullName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    password: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    phoneNumber: Yup.string()
+      .matches(/^\d{10}$/, 'Phone number must be 10 digits')
+      .required('Required'),
+    role: Yup.string(),
+    licenseNumber: Yup.string(),
+    
+  vehicleNumber: Yup.string(),
+   
+  vehicleType: Yup.string(),
+  }))
 
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+  
+
+  
   const registerUser = async (values) => {
     values.role = isRider ? 'rider' : 'user'
-    var formData = new FormData();
-
-    const keys =Object.keys(values)
-    keys.forEach((item)=>{
-      formData.append(item,values[item] );
-    })
-    formData.append('avatar',file );
     const requestOptions = {
       method: 'POST',
-      body: formData
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
     };
+
     try {
       const res = await fetch('http://localhost:4000/register', requestOptions)
-   
+      console.log(res)
+      const data = await res.json()
+      console.log(data)
+      if (res && data.success) {
+        messageApi.success(data.msg);
+      } else {
+        messageApi.error(data.msg);
+      }
     } catch (err) {
-      // error tracking tools
-      console.log(err)
+      messageApi.warning(data.msg);
     }
   };
 
   const switchRider = (checked) => {
     setisRider(checked);
-  }
-
-
-  const handleFileSave =(e)=> {
-    console.log(e.target.files)
-    setFile(e.target.files[0])
   }
 
   return (
@@ -77,6 +100,7 @@ const Register = () => {
           registerUser(values);
         }}
       >
+
         {({ errors, touched }) => (
           <Form className={styles.Home_form}>
             <p className={styles.formTitle}>Signup</p>
@@ -85,32 +109,46 @@ const Register = () => {
             <div className={styles.switch_user}>
             <Field name="fullName" placeholder="Full Name" className={styles.Home_input} />
               {errors.fullName && touched.fullName ? (
-                <div>{errors.fullName}</div>
+                <div className={styles.required}>{errors.fullName}</div>
               ) : null}
                 <Field name="phoneNumber" placeholder="Phone Number" className={styles.Home_input} />
-              {errors.phoneNumber && touched.phoneNumber ? <div>{errors.phoneNumber}</div> : null}
+              {errors.phoneNumber && touched.phoneNumber ? <div className={styles.required}>{errors.phoneNumber}</div> : null}
 
-              <Field name="password" placeholder="Password" className={styles.Home_input} />
+              <Field name="password"  type={open == false ? "password" : "text"} placeholder="Password" className={styles.Home_input} />
+              <div className={styles.showpass}>
+                {open == false ? (
+                  <RxEyeClosed onClick={toggle} />
+                ) : (
+                  <RxEyeOpen onClick={toggle} />
+                )}
+              </div>
               {errors.password && touched.password ? (
-                <div>{errors.password}</div>
+                <div className={styles.required}>{errors.password}</div>
               ) : null}
-              <Field name="confirmPassword" placeholder="Confirm password" className={styles.Home_input} />
+              <Field name="confirmPassword"  type={open == false ? "password" : "text"} placeholder="Confirm password" className={styles.Home_input} />
+              <div className={styles.showpass}>
+                {open == false ? (
+                  <RxEyeClosed onClick={toggle} />
+                ) : (
+                  <RxEyeOpen onClick={toggle} />
+                )}
+              </div>
               {errors.confirmPassword && touched.confirmPassword ? (
-                <div>{errors.confirmPassword}</div>
+                <div className={styles.required}>{errors.confirmPassword}</div>
               ) : null}
               <div>
                 <Switch 
-                defaultChecked={true} 
-                style={{backgroundColor:isRider? 'blue':'red'}}
+                defaultChecked={false} 
+                style={{backgroundColor:isRider? 'blue':'#7b6a6a'}}
                 onChange={switchRider} />
                 <span>Rider</span>
               </div>
               {isRider ? (
                 <div>
                   <Field name="licenseNumber" placeholder="License No" className={styles.Home_input} />
-                  {errors.licenseNumber && touched.licenseNumber ? <div>{errors.licenseNumber}</div> : null}
+                  {errors.licenseNumber && touched.licenseNumber ? <div className={styles.required}>{errors.licenseNumber}</div> : null}
                   <Field name="vehicleNumber" placeholder="Vehicle Number" className={styles.Home_input} />
-                  {errors.vehicleNumber && touched.vehicleNumber ? <div>{errors.vehicleNumber}</div> : null}
+                  {errors.vehicleNumber && touched.vehicleNumber ? <div className={styles.required}>{errors.vehicleNumber}</div> : null}
                   <Field
                     name="vehicleType"
                     className={styles.Home_select}
@@ -122,11 +160,10 @@ const Register = () => {
                     <option value="car">Car</option>
                     <option value="motorcycle">Motorcycle</option>
                   </Field>
-                  {errors.vehicleType && touched.vehicleType ? <div>{errors.vehicleType}</div> : null}
+                  {errors.vehicleType && touched.vehicleType ? <div className={styles.required}>{errors.vehicleType}</div> : null}
                   </div>
               ) : null}
             </div>
-            <input type="file" onChange={handleFileSave}/>
             <button type="submit" className={styles.submitButton}>Submit</button>
             Already have an account? <Link href="/">Login</Link>
           </Form>
@@ -136,5 +173,3 @@ const Register = () => {
 }
 
 export default Register;
-
-
